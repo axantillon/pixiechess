5e80609741908cd7022460a7e497549054dacd09
axantillon <aantillon.leiva@gmail.com>
2023-09-28 11:51:01 -0600

    finish UI buttons

--- src/components/PieceCarousel.tsx
+++ src/components/PieceCarousel.tsx
@@ -5,6 +5,48 @@ import Image from 'next/image';
 import { FC, useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
 import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+const PieceBox = ({ piece }: { piece: Piece }) => {
+
+    return (
+        <div className={cn("flex w-full h-[500px] font-extrabold px-12 py-6 border-2 border-l-white border-t-white border-r-black border-b-black bg-[#F3F8FB]/80")}>
+                <div className="w-2/3 flex flex-col space-y-8">
+
+                    <div className="h-1/2 flex flex-col space-y-2">
+                        <span className='text-3xl'>#{piece.id} {piece.name}</span>
+                        <span>{piece.dateCreated}</span>
+                        <span className=''>{piece.description}</span>
+                        <span>{piece.category}</span>
+                    </div>
+
+                    <div className="h-1/6 flex text-2xl font-bold space-x-10">
+                        <div className="flex flex-col space-y-1">
+                            <span>Current bid</span>
+                            <span>Ξ{piece.currentBid}</span>
+                        </div>
+                        <div className="flex flex-col space-y-1">
+                            <span>Auction ends in</span>
+                            <span>{piece.auctionEndDate}</span>
+                        </div>
+                    </div>
+
+                    <div className="h-1/6 flex items-center space-x-2">
+                        <Input placeholder={`${piece.currentBid} or more`} className='w-1/2 text-xl font-bold' />
+                        <div className="diagonal-corners text-white px-4 bg-[#EC7FFB] z-10 leading-4 drop-shadow-md cursor-pointer">
+                            <span>Place bid</span>
+                        </div>
+                    </div>
+
+                    <div className="h-1/4"></div>
+
+                </div>
+                <div className="w-1/3 py-8">
+                    <Image src={piece.imageMovement} alt='' />
+                </div>
+            </div>
+    )
+}
 
 interface PieceCarouselProps {
   pieces: Piece[];
@@ -22,7 +64,7 @@ const PieceCarousel: FC<PieceCarouselProps> = ({ pieces }) => {
 
             <div className="flex-1 h-full flex flex-nowrap items-center justify-evenly overflow-hidden ">
                 {pieces.map((piece, index) => (
-                    <div onClick={() => {setActivePieceIndex(index)}} key={index} className={cn("mx-8 p-2 flex-shrink-0 cursor-pointer border border-transparent rounded-lg", activePieceIndex === index ? 'w-44 h-56 border-black/50' : 'w-28 h-36 hover:border-black/20')}>
+                    <div onClick={() => {setActivePieceIndex(index)}} key={index} className={cn("mx-8 p-2 flex-shrink-0 cursor-pointer border border-transparent rounded-lg transition-widthandheight ease-in-out delay-50", activePieceIndex === index ? 'w-44 h-56 border-black/50' : 'w-28 h-36 hover:border-black/20')}>
                         <div className="relative w-full h-full">
                             <Image src={piece.imagePiece} alt='' fill style={{ objectFit: 'contain'}}/>
                             <div className="absolute -bottom-2 right-1">
@@ -38,31 +80,7 @@ const PieceCarousel: FC<PieceCarouselProps> = ({ pieces }) => {
             </Button>
         </div>
         <div className="w-full px-36">
-            <div className={cn("flex w-full max-h-[1000px] font-extrabold px-12 py-6 border-2 border-l-white border-t-white border-r-black border-b-black bg-[#F3F8FB]/80")}>
-                <div className="w-2/3 flex flex-col space-y-8">
-
-                    <div className="flex flex-col space-y-2">
-                        <span className='text-3xl'>#{pieces[activePieceIndex].id} {pieces[activePieceIndex].name}</span>
-                        <span>{pieces[activePieceIndex].dateCreated}</span>
-                        <span className=''>{pieces[activePieceIndex].description}</span>
-                        <span>{pieces[activePieceIndex].category}</span>
-                    </div>
-
-                    <div className="flex text-2xl font-bold">
-                        <div className="flex flex-col space-y-1">
-                            <span>Current Bid</span>
-                            <span>Ξ{pieces[activePieceIndex].currentBid}</span>
-                        </div>
-                        <div className="flex flex-col space-y-1">
-
-                        </div>
-                    </div>
-
-                </div>
-                <div className="w-1/3 py-8">
-                    <Image src={pieces[activePieceIndex].imageMovement} alt='' />
-                </div>
-            </div>
+            <PieceBox piece={pieces[activePieceIndex]} />
         </div>
     </>)
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      transitionProperty: {
+        "widthandheight": "width, height",
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
