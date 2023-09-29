75a630678e2cb444f3cd9ee90a1d75f70615148d
axantillon <aantillon.leiva@gmail.com>
2023-09-29 12:42:27 -0600

    add the dream arrow

--- src/components/PieceCarousel.tsx
+++ src/components/PieceCarousel.tsx
@@ -10,7 +10,7 @@ import { Input } from './ui/input';
 const PieceBox = ({ piece }: { piece: Piece }) => {
 
     return (
-        <div className={cn("flex w-full max-w-[800px] h-[500px] mx-auto font-extrabold px-12 py-6 border-2 border-l-white border-t-white border-r-black border-b-black bg-[#F3F8FB]/50")}>
+        <div className={cn("flex w-full h-full font-extrabold px-12 py-6 border-2 border-l-white border-t-white border-r-black border-b-black bg-[#F3F8FB]/50")}>
                 <div className="w-2/3 flex flex-col space-y-8">
 
                     <div className="h-1/2 flex flex-col space-y-2">
@@ -52,19 +52,28 @@ interface PieceCarouselProps {
   pieces: Piece[];
 }
 
+const arrowPosition = [
+    'before:left-[158px]',
+    'before:left-[258px]',
+    'before:left-[358px]',
+    'before:left-[458px]',
+    'before:left-[558px]',
+]
+
 const PieceCarousel: FC<PieceCarouselProps> = ({ pieces }) => {
 
-    const [activePieceIndex, setActivePieceIndex] = useState<number>(1);
+    const [activePieceIndex, setActivePieceIndex] = useState<number>(0);
 
-    return (<>
-        <div className={'flex items-center justify-center w-full h-72 px-14 space-x-10'}>
+    return (
+    <div className='max-w-[800px] mx-auto px-6'>
+        <div className={'flex items-center justify-center h-72 space-x-10 '}>
             <Button onClick={() => setActivePieceIndex(Math.max(0, activePieceIndex - 1))} className='text-2xl'>
                 <FaArrowLeft />
             </Button>
 
-            <div className="flex-1 h-full flex flex-nowrap items-center justify-evenly overflow-hidden ">
+            <div className="w-full h-full flex flex-nowrap items-center justify-evenly overflow-hidden ">
                 {pieces.map((piece, index) => (
-                    <div onClick={() => {setActivePieceIndex(index)}} key={index} className={cn("p-2 cursor-pointer border border-transparent rounded-lg transition-widthandheight ease-in-out delay-50", activePieceIndex === index ? 'w-44 h-56 border-black/50' : 'w-28 h-36 hover:border-black/20')}>
+                    <div onClick={() => {setActivePieceIndex(index)}} key={index} className={cn("p-2 cursor-pointer border border-transparent rounded-lg transition-widthandheight ease-in-out delay-50", activePieceIndex === index ? 'w-36 h-48 border-black/50' : 'w-20 h-28 hover:border-black/20')}>
                         <div className="relative w-full h-full">
                             <Image src={piece.imagePiece} alt='' fill style={{ objectFit: 'contain'}}/>
                             <div className="absolute -bottom-2 right-1">
@@ -79,10 +88,11 @@ const PieceCarousel: FC<PieceCarouselProps> = ({ pieces }) => {
                 <FaArrowRight />
             </Button>
         </div>
-        <div className="w-full px-36">
+        <div className={cn("relative w-full h-[500px] before:content-[''] before:absolute before:w-8 before:h-8 before:-top-4 before:rotate-45 before:border-l-2 before:border-l-white before:bg-[#F3F8FB]/50 before:-z-1", arrowPosition[activePieceIndex])}>
             <PieceBox piece={pieces[activePieceIndex]} />
         </div>
-    </>)
+    </div>
+    )
 }
 
 export default PieceCarousel;
